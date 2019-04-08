d8d9c7b69a992af4b03b9f7f7def144a6a3d87ab
Kevin Sousselier <ksousselier@fuwear.fwrc>
2019-04-08 22:02:22 +0200

    Create project

---
+++ AuthorizationServer/Config.cs
@@ -0,0 +1,33 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace AuthorizationServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("scope.FullAccess", "RoomBooking API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "ClientId",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "scope.FullAccess" }
+                }
+            };
+        }
+    }
+}
---
+++ AuthorizationServer/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthorizationServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddSigningCredential(GetRandomCertificate())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private static X509Certificate2 GetRandomCertificate()
+        {
+            X509Store st = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+            st.Open(OpenFlags.ReadOnly);
+            try
+            {
+                var certCollection = st.Certificates;
+
+                if (certCollection.Count == 0)
+                {
+                    return null;
+                }
+                return certCollection[0];
+            }
+            finally
+            {
+                st.Close();
+            }
+        }
+    }
+}
